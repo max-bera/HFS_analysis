ae59ea619a44b6c4476574df81ca95640292dc52
Massimiliano Berardi <massimilianoberardi@massimiianosmbp.home>
2023-04-14 08:45:17 +0200

    refined sensor finding routine

--- Spectral_analysis/main_spectral_analysis.m
+++ Spectral_analysis/main_spectral_analysis.m
@@ -1,7 +1,7 @@
-path = 'C:\Users\massimiliano.berardi\Desktop\test_data\HCV29_3\20220324_155747_S5_01.tdms';
-path_bg = 'C:\Users\massimiliano.berardi\Desktop\test_data\20220323_112003_background_01.tdms';
-resampling_freq = 100; %Hz, max = acq_freq
-padding = 5; %n times the original array length
+path = '/Users/massimilianoberardi/Desktop/Spheroids_results/CONTROL_3_14/HCV_DAY3/20220324_145955_S2_01.tdms';
+path_bg = '/Users/massimilianoberardi/Desktop/Spheroids_results/20220323_112003_background_01.tdms';
+resampling_freq = 500; %Hz, max = acq_freq
+padding = 2; %n times the original array length
 pressure_sens = 76.50; %Pa/nm
 pip_rad = 25e-6;
 
@@ -29,32 +29,46 @@ time_array = linspace(0,length(displacement_field)/resampling_freq,length(displa
 % isolate pressure sensor and sample via intensity analysis
 [spectrum_k_noBG, ~] = k_resample_M(spectrum_noBG_lambda,k_space);
 [cavity_data_noBG, ~] = kspace_to_cavity(spectrum_k_noBG,k_even_spacing,padding);
-%%
+
 [idx_pressure_cavity, idxs_sample_cavity] = find_sensors(cavity_data_noBG);
 
+opti_cross_sec = (idxs_sample_cavity(2) - idxs_sample_cavity(1)) * cavity_distance(2);
 %% extract pressure
 
 pressure_signal = displacement_field(:,idx_pressure_cavity)*pressure_sens-...
     displacement_field(1,idx_pressure_cavity)*pressure_sens;
+%make it nice
+freq = 60; %cutoff
+     Order = 5;
+     [b, a] = butter(Order, freq / (resampling_freq * 2), 'low');
+     pressure_signal = filter(b,a,pressure_signal);
 
 %% extract displacement
-displ_signal = displacement_field(:,idxs_sample_cavity(1):idxs_sample_cavity(2)+5)/1.45 - ...\
-    displacement_field(1,idxs_sample_cavity(1):idxs_sample_cavity(2)+5)/1.45;
+displ_signal = displacement_field(:,idxs_sample_cavity(1):idxs_sample_cavity(2))/1.45 - ...
+    displacement_field(1,idxs_sample_cavity(1):idxs_sample_cavity(2))/1.45;
+displ_signal = filter(b,a,displ_signal);
 
+K = (1/4)*ones(3);
+displ_signal = conv2(displ_signal(1:1:end,:),K,'same');
 figure(10)
-subplot(1,2,1)
+subplot(1,2,1),
 plot(time_array,pressure_signal,'k')
 grid on
 xlabel('time [s]')
 ylabel('pressure [Pa]')
+%limit plot to interesting part, i.e. 1.8 times pip_rad
+idx_decay = find(cavity_distance/1.45>pip_rad*4*1e6*1.45,1,'first')
 subplot(1,2,2)
 hold off
-for i = 1000:50:1000
-    plot3(linspace(i,i,size(displ_signal,2))/resampling_freq,linspace(1,cavity_distance(2)*size(displ_signal,2),size(displ_signal,2)),displ_signal(i,:))
-    xlabel('time [s]')
-    ylabel('distance [\mum]')
-    zlabel('displacement [nm]')
+j=1;
+for i = 0.1*resampling_freq:resampling_freq/10:20*resampling_freq
+    plot(cavity_distance(1:idx_decay)/1.45,displ_signal(i,1:idx_decay),'k.-')
+    ptoP(j) = mean(displ_signal(i,20:25))/max(displ_signal(i,1:idx_decay));
+    j = j+1;
+    xlabel('distance [\mum]')
+    ylabel('displacement [nm]')
     hold on
     grid on
 end
-zlim([0 10000])
+%
+% zlim([0 10000])
